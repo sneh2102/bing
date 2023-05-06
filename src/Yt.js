737423b335bbbcaf4fdc7b89d819909c07e8d9b3
Sneh Patel <patel.sneh2102@gmail.com>
2023-05-06 08:13:56 +0530

    second commit

--- src/Yt.js
+++ src/Yt.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Yt = () => {
   const handleClick = () => {
-    for (let i = 0; i < 30; i++) {
-      const randomSearch = Math.random().toString(36).substring(2);
+    for (let i = 0; i < 60; i++) {
+      const randomSearch = Math.random().toString(30).substring(2);
       window.open(`https://www.bing.com/search?q=${randomSearch}`, '_blank');
     }
   }
